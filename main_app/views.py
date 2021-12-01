d6fea8726cc8cfa2cd8133cf9fb7b9b0cafc7474
Jrandle10 <91491549+Jrandle10@users.noreply.github.com>
2021-12-01 15:49:10 -0700

    finish part 3 of finch collector

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 # Class and List below
@@ -19,4 +20,14 @@ def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   return render(request, 'finches/detail.html', { 'finch': finch })
 
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
 
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches/'
